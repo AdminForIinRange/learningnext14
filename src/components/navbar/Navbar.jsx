dde074afc1b3b5abe1fca41f473b60385b660749
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-31 16:28:12 +1030

    estblaihing links
    import Link from "next/link";
    import React from "react";
    
    const Links = () => {
      const links = [
        {
          title: "Homepage",
          path: "/",
        },
        {
          title: "About",
          path: "/about",
        },
        {
          title: "Contact",
          path: "/contact",
        },
        {
          title: "Blog",
          path: "/blog",
        },
      ];
    
      return (
        <div>
          {links.map((link => (
            <Link  key={link.title} href={link.path}>
              {link.title}
            </Link>
          )))}
        </div>
      );
    };
    
    export default Links;

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+import Links from "./links/Links";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div>Nav</div>
+      <Links />
+    </div>
+  );
+};
+
+export default Navbar;
