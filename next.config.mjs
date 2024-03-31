c8e3bba643c20db7d8913a0828f4c866715c098a
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-31 18:59:59 +1030

    applying protocol
    /** @type {import('next').NextConfig} */
    const nextConfig = {
        images: {
    
            remotePatterns:[
                {
                    protocol: 'https',
                    hostname: 'www.pexel.com',
                }
            ]
        }
    };
    
    export default nextConfig;
    
    and changing logo to be link to go back to home

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        
+        remotePatterns:[
+            {
+                protocol: 'https',
+                hostname: 'www.pexel.com',
+            }
+        ]
+    }
+};
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import React from 'react';
-
+import styles from "./about.module.css"
 const AboutPage = () => {
   return (
-    <div>
+        <div >
+      <div>
+
       {/* Use the imported image directly */}
-      <Image src="/about.png" alt="about" width={500} height={500} />
+      <Image className={styles.img} src="/about.png" alt="about" width={500} height={500} />
+      </div>
     </div>
   );
 };
