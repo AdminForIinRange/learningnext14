800cec5a98935676ff02d729dc0f4535426aabed
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-03-31 18:00:27 +1030

    {/*   So he is just pushing a new obj in the item, it's like pushing a conditional into an array.   There are different ways of doing this, just like that 'active' parameter thing in NavLinks. */}

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import styles from "./links.module.css"
+import styles from "./links.module.css";
 import NavLinks from "./navLink/navLinks";
 const Links = () => {
   const links = [
@@ -22,16 +22,34 @@ const Links = () => {
     },
   ];
 
-  const session = true
-  const isAdmin = true
+  const session = true;
+  const isAdmin = true;
 
   return (
-    <div  className={styles.links}>
-      {links.map((link => (
-        <Link  key={link.title} href={link.path}>
-<NavLinks item={link} key={link.title}/>
+    <div className={styles.links}>
+      {links.map((link) => (
+        <Link key={link.title} href={link.path}>
+          <NavLinks item={link} key={link.title} />
         </Link>
-      )))}
+      ))}
+
+      {session ? (
+        <>
+
+   {/* 
+  So he is just pushing a new obj in the item, it's like pushing a conditional into an array. 
+  There are different ways of doing this, just like that 'active' parameter thing in NavLinks.
+*/}
+
+          {isAdmin && <NavLinks item={{ title: "Admin", path: "/admin" }} />} 
+          <button className={styles.logout}>Logout</button>{" "}
+        </>
+      ) : (
+        <>
+
+          <NavLinks item={{ title: "Login", path: "/login" }} />{" "}
+        </>
+      )}
     </div>
   );
 };
