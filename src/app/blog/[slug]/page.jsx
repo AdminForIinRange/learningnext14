c573c1d19d84625116f116a06f6300a60408328c
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-01 21:21:33 +1030

    added
    export const generateMetadata = async ({ params }) => {
      const { slug } = params;
    
      const post = await getPost(slug);
    
      return {
        title: post.title,
        description: post.desc,
      };
    };

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -13,6 +13,24 @@ import { getPost } from "@/lib/data";
 //   return res.json();
 // };
 
+
+
+
+export const generateMetadata = async ({ params }) => {
+  const { slug } = params;
+
+  const post = await getPost(slug);
+
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+};
+
+
+
+
+
 const SinglePostPage = async ({ params }) => {
   // using params, slug is passed as a parameter its a buit in function in nextjs
 
